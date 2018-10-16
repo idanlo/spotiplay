3182fb39f4d2e328f4ba9c59b8d1e72e239836db
idanlo <idan2110@gmail.com>
2018-10-16 14:42:18 +0300

    Update PlaylistView component to use my react-spotify-api library

--- src/Components/PlaylistView/PlaylistView.jsx
+++ src/Components/PlaylistView/PlaylistView.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import * as actionTypes from "../../store/actions/actionTypes";
-import withSpotifyApi from "../../HOC/withSpotifyApi";
 import { connect } from "react-redux";
+import { Playlist } from "react-spotify-api";
 import Vibrant from "node-vibrant";
 import {
     Grid,
@@ -14,72 +14,31 @@ import {
     ListItemIcon
 } from "@material-ui/core";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import { TrackDetailsLink } from "../UI/TrackDetailsLink";
 import PauseIcon from "@material-ui/icons/Pause";
 
 class PlaylistView extends Component {
-    state = {
-        playlist: null
-    };
-
-    shouldComponentUpdate(nextProps, nextState) {
-        return (
-            (this.state.playlist &&
-                nextState.playlist &&
-                this.state.playlist.id !== nextState.playlist.id) ||
-            (!this.state.playlist && nextState.playlist) ||
-            this.props.match.params.id !== nextProps.match.params.id ||
-            this.props.currentlyPlaying !== nextProps.currentlyPlaying ||
-            this.props.isPlaying !== nextProps.isPlaying
-        );
-    }
-
-    componentDidMount() {
-        if (
-            this.props.api &&
-            (!this.state.playlist ||
-                this.state.playlist.id !== this.props.match.params.id)
-        ) {
-            this.getPlaylist(this.props.match.params.id);
-        }
-    }
-
-    componentDidUpdate() {
-        if (
-            this.props.api &&
-            (!this.state.playlist ||
-                this.state.playlist.id !== this.props.match.params.id)
-        ) {
-            this.getPlaylist(this.props.match.params.id);
-        }
-    }
-
-    getPlaylist = id => {
-        if (id && this.props.api) {
-            const { api } = this.props;
-            api.getPlaylist(id).then(res => {
-                this.setState({ playlist: res });
-                Vibrant.from(res.images[0].url)
-                    .getPalette()
-                    .then(palette => {
-                        let rgb = palette.DarkMuted._rgb.join(", ");
-                        let color = "rgb(" + rgb + ")";
-                        let bgImage = `linear-gradient(${color}, rgb(6, 9, 10) 85%)`;
-                        this.props.setBackgroundImage(bgImage);
-                    });
+    setBackgroundImage = url => {
+        Vibrant.from(url)
+            .getPalette()
+            .then(palette => {
+                let rgb = palette.DarkMuted._rgb.join(", ");
+                let color = "rgb(" + rgb + ")";
+                let bgImage = `linear-gradient(${color}, rgb(6, 9, 10) 85%)`;
+                this.props.setBackgroundImage(bgImage);
             });
-        }
     };
 
-    playSongHandler = track => {
-        if (this.state.playlist) {
+    playSongHandler = (track, playlist) => {
+        if (track) {
             let uris;
-            if (track === "playlist") {
+            if (track.type === "playlist") {
                 uris = JSON.stringify({
-                    context_uri: this.state.playlist.uri
+                    context_uri: playlist.uri
                 });
             } else {
                 uris = JSON.stringify({
-                    context_uri: this.state.playlist.uri,
+                    context_uri: playlist.uri,
                     offset: {
                         uri: track.uri
                     }
@@ -90,98 +49,147 @@ class PlaylistView extends Component {
     };
 
     render() {
-        const { playlist } = this.state;
-
-        let mainContent = null;
-
-        if (playlist) {
-            mainContent = (
-                <Grid container>
-                    <Grid item xs={12} md={4}>
-                        <div style={{ textAlign: "center" }}>
-                            <img
-                                src={playlist.images[0].url}
-                                style={{
-                                    width: "70%",
-                                    height: "70%",
-                                    display: "block",
-                                    margin: "30px auto"
-                                }}
-                                alt="Playlist"
-                            />
-                            <Typography variant="title">
-                                {playlist.name}
-                            </Typography>
-                            <Typography
-                                variant="subheading"
-                                color="textSecondary"
-                            >
-                                {playlist.owner.display_name}
-                            </Typography>
-                            <Typography
-                                variant="subheading"
-                                color="textSecondary"
-                            >
-                                {playlist.tracks.items.length + " songs"}
-                            </Typography>
-                            <Button
-                                color="primary"
-                                onClick={() => this.playSongHandler("playlist")}
-                            >
-                                Play
-                            </Button>
-                        </div>
-                    </Grid>
-                    <Grid item xs={12} md={8}>
-                        <List style={{ width: "100%" }}>
-                            {this.state.playlist.tracks.items.map(track => (
-                                <ListItem
-                                    key={track.track.id}
-                                    style={
-                                        this.props.currentlyPlaying ===
-                                            track.track.name &&
-                                        this.props.isPlaying
-                                            ? { background: "#1db954" }
-                                            : null
-                                    }
-                                >
-                                    <ListItemIcon style={{ cursor: "pointer" }}>
-                                        {this.props.currentlyPlaying ===
-                                            track.track.name &&
-                                        this.props.isPlaying ? (
-                                            <PauseIcon
-                                                onClick={this.props.pauseSong}
-                                            />
-                                        ) : (
-                                            <PlayArrowIcon
-                                                onClick={() =>
-                                                    this.playSongHandler(
-                                                        track.track
-                                                    )
-                                                }
-                                            />
-                                        )}
-                                    </ListItemIcon>
-                                    <ListItemText
-                                        primary={track.track.name}
-                                        secondary={
-                                            track.track.artists
-                                                .map(artist => artist.name)
-                                                .join(", ") +
-                                            " • " +
-                                            track.track.album.name
+        return (
+            <Playlist id={this.props.match.params.id}>
+                {playlist =>
+                    playlist ? (
+                        <Grid container>
+                            <Grid item xs={12} md={4}>
+                                <div style={{ textAlign: "center" }}>
+                                    <img
+                                        src={playlist.images[0].url}
+                                        style={{
+                                            width: "70%",
+                                            height: "70%",
+                                            display: "block",
+                                            margin: "30px auto"
+                                        }}
+                                        alt="Playlist"
+                                        onLoad={() =>
+                                            this.setBackgroundImage(
+                                                playlist.images[0].url
+                                            )
+                                        }
+                                    />
+                                    <Typography variant="title">
+                                        {playlist.name}
+                                    </Typography>
+                                    <Typography
+                                        variant="subheading"
+                                        color="textSecondary"
+                                    >
+                                        {playlist.owner.display_name}
+                                    </Typography>
+                                    <Typography
+                                        variant="subheading"
+                                        color="textSecondary"
+                                    >
+                                        {playlist.tracks.items.length +
+                                            " songs"}
+                                    </Typography>
+                                    <Button
+                                        color="primary"
+                                        onClick={() =>
+                                            this.playSongHandler(playlist)
                                         }
                                     >
-                                        {track.track.name}
-                                    </ListItemText>
-                                </ListItem>
-                            ))}
-                        </List>
-                    </Grid>
-                </Grid>
-            );
-        }
-        return mainContent;
+                                        Play
+                                    </Button>
+                                </div>
+                            </Grid>
+                            <Grid item xs={12} md={8}>
+                                <List style={{ width: "100%" }}>
+                                    {playlist.tracks.items.map(track => {
+                                        let ArtistAlbumLink = (
+                                            <React.Fragment>
+                                                {track.track.artists.map(
+                                                    (artist, index) => (
+                                                        <React.Fragment
+                                                            key={artist.id}
+                                                        >
+                                                            <TrackDetailsLink
+                                                                to={`/artist/${
+                                                                    artist.id
+                                                                }`}
+                                                            >
+                                                                {artist.name}
+                                                            </TrackDetailsLink>
+                                                            {index !==
+                                                            track.track.artists
+                                                                .length -
+                                                                1
+                                                                ? ", "
+                                                                : null}
+                                                        </React.Fragment>
+                                                    )
+                                                )}
+                                                <span> • </span>
+                                                <TrackDetailsLink
+                                                    to={`/album/${
+                                                        track.track.album.id
+                                                    }`}
+                                                >
+                                                    {track.track.album.name}
+                                                </TrackDetailsLink>
+                                            </React.Fragment>
+                                        );
+                                        return (
+                                            <ListItem
+                                                key={track.track.id}
+                                                style={
+                                                    this.props
+                                                        .currentlyPlaying ===
+                                                        track.track.name &&
+                                                    this.props.isPlaying
+                                                        ? {
+                                                              background:
+                                                                  "#1db954"
+                                                          }
+                                                        : null
+                                                }
+                                            >
+                                                <ListItemIcon
+                                                    style={{
+                                                        cursor: "pointer"
+                                                    }}
+                                                >
+                                                    {this.props
+                                                        .currentlyPlaying ===
+                                                        track.track.name &&
+                                                    this.props.isPlaying ? (
+                                                        <PauseIcon
+                                                            onClick={
+                                                                this.props
+                                                                    .pauseSong
+                                                            }
+                                                        />
+                                                    ) : (
+                                                        <PlayArrowIcon
+                                                            onClick={() =>
+                                                                this.playSongHandler(
+                                                                    track.track,
+                                                                    playlist
+                                                                )
+                                                            }
+                                                        />
+                                                    )}
+                                                </ListItemIcon>
+                                                <ListItemText
+                                                    primary={track.track.name}
+                                                    secondary={ArtistAlbumLink}
+                                                >
+                                                    {track.track.name}
+                                                </ListItemText>
+                                            </ListItem>
+                                        );
+                                    })}
+                                </List>
+                            </Grid>
+                        </Grid>
+                    ) : null
+                }
+            </Playlist>
+        );
     }
 }
 
@@ -209,5 +217,5 @@ export default withRouter(
     connect(
         mapStateToProps,
         mapDispatchToProps
-    )(withSpotifyApi(PlaylistView))
+    )(PlaylistView)
 );
