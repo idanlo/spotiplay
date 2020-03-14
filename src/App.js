2faf9ea8a8f19dbbb6cd4f3ca32a86c8d6a1fce1
Idan Lottan <idan2110@gmail.com>
2020-03-14 13:56:44 +0200

    format and remove unneeded space in App.js

--- src/App.js
+++ src/App.js
@@ -4,42 +4,41 @@ import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline/CssBaseline';
 
 class App extends Component {
-    render() {
-        const theme = createMuiTheme({
-            palette: {
-                type: 'dark',
+  render() {
+    const theme = createMuiTheme({
+      palette: {
+        type: 'dark',
+        primary: {
+          light: '#757ce8',
+          main: '#1db954',
+          dark: '#191414',
+          contrastText: '#fff'
+        },
+        secondary: {
+          light: '#ff7961',
+          main: '#ffffff',
+          dark: '#191414',
+          contrastText: '#000'
+        }
+      },
+      typography: {
+        useNextVariants: true
+      }
+    });
 
-                primary: {
-                    light: '#757ce8',
-                    main: '#1db954',
-                    dark: '#191414',
-                    contrastText: '#fff'
-                },
-                secondary: {
-                    light: '#ff7961',
-                    main: '#ffffff',
-                    dark: '#191414',
-                    contrastText: '#000'
-                }
-            },
-            typography: {
-                useNextVariants: true
-            }
-        });
-
-        return (
-            <MuiThemeProvider theme={theme}>
-                <CssBaseline>
-                    <Layout
-                        style={{
-                            fontFamily:
-                                'spotify-circular,Helvetica Neue,Helvetica,Arial,Hiragino Kaku Gothic Pro,Meiryo,MS Gothic,sans-serif'
-                        }}
-                    />
-                </CssBaseline>
-            </MuiThemeProvider>
-        );
-    }
+    return (
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline>
+          <Layout
+            style={{
+              fontFamily:
+                'spotify-circular,Helvetica Neue,Helvetica,Arial,Hiragino Kaku Gothic Pro,Meiryo,MS Gothic,sans-serif'
+            }}
+          />
+        </CssBaseline>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
