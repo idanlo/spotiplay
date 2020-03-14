b2b7cfcc117a9d3e07e6ed42f69b059953cb71b6
Idan Lottan <idan2110@gmail.com>
2020-03-14 13:57:08 +0200

    utils - export logger object, that has a 'log' function that logs to the console only of the node environment is 'development'

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,13 @@
 export const milisToMinutesAndSeconds = mil => {
-    let minutes = Math.floor(mil / 60000);
-    let seconds = ((mil % 60000) / 1000).toFixed(0);
-    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  let minutes = Math.floor(mil / 60000);
+  let seconds = ((mil % 60000) / 1000).toFixed(0);
+  return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+};
+
+export const logger = {
+  log: (...args) => {
+    if (process.env.NODE_ENV === 'development') {
+      console.log(...args);
+    }
+  }
 };
