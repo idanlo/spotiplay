506b9b38325e56bdbf35204d9467d83cfb063010
Idan Lottan <idan2110@gmail.com>
2020-03-14 14:00:27 +0200

    Login - redirect to new backend page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,27 +2,21 @@ import React from 'react';
 import { Grid, Typography } from '@material-ui/core';
 
 const Login = () => (
-    <Grid container>
-        <Grid item style={{ margin: '0 auto' }}>
-            {/* <a
+  <Grid container>
+    <Grid item style={{ margin: '0 auto' }}>
+      {/* <a
                 href="http://spotify-test-backend.herokuapp.com/login"
                 style={{ textDecoration: "none" }}
             > */}
-            {/* prod */}
-            <a
-                href={`https://accounts.spotify.com/authorize?client_id=${
-                    process.env.REACT_APP_SPOTIFY_CLIENT_ID
-                }&redirect_uri=${
-                    process.env.REACT_APP_SPOTIFY_REDIRECT_URI
-                }&scope=${
-                    process.env.REACT_APP_SPOTIFY_SCOPE
-                }&response_type=token`}
-                style={{ textDecoration: 'none' }}
-            >
-                <Typography variant="h1">Login with Spotify</Typography>
-            </a>
-        </Grid>
+      {/* prod */}
+      <a
+        style={{ textDecoration: 'none' }}
+        href={'https://spotiplay-backend.herokuapp.com/login'}
+      >
+        <Typography variant="h1">Login with Spotify</Typography>
+      </a>
     </Grid>
+  </Grid>
 );
 
 export default Login;
