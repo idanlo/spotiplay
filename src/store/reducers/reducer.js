36c64d2901115dd042885a31b40e069ddc394069
Idan Lottan <idan2110@gmail.com>
2020-03-17 13:40:58 +0200

    reducer - format, change SET_USER action to be able to accept only some of the properties of the user and merge it with the current value in the store

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -1,69 +1,72 @@
-import * as actionTypes from "../actions/actionTypes";
+import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
-    current_user: null,
-    recently_played: null,
-    play_now: {
-        type: null,
-        uri: null
-    },
-    currently_playing: null,
-    isPlaying: false,
-    backgroundImage: "linear-gradient(rgb(58, 91, 95), rgb(6, 9, 10) 85%)"
+  current_user: null,
+  recently_played: null,
+  play_now: {
+    type: null,
+    uri: null
+  },
+  currently_playing: null,
+  isPlaying: false,
+  backgroundImage: 'linear-gradient(rgb(58, 91, 95), rgb(6, 9, 10) 85%)'
 };
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case actionTypes.SET_USER:
-            return {
-                ...state,
-                isLoggedIn: true,
-                current_user: action.user
-            };
-        case actionTypes.SET_RECENTLY_PLAYED:
-            return {
-                ...state,
-                recently_played: action.recently_played
-            };
-        case actionTypes.SET_PLAY_NOW:
-            return {
-                ...state,
-                play_now: {
-                    type: action.uri_type,
-                    uri: action.uri
-                }
-            };
-        case actionTypes.RESET_PLAY_NOW:
-            return {
-                ...state,
-                play_now: {
-                    type: null,
-                    uri: null
-                }
-            };
-        case actionTypes.SET_CURRENTLY_PLAYING:
-            return {
-                ...state,
-                currently_playing: action.song
-            };
-        case actionTypes.SET_IS_PLAYING:
-            return {
-                ...state,
-                isPlaying: action.isPlaying
-            };
-        case actionTypes.SET_BACKGROUND_IMAGE:
-            return {
-                ...state,
-                backgroundImage: action.backgroundImage
-            };
-        case actionTypes.PLAY_SONG_START:
-            return {
-                ...state,
-                play_now: null
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case actionTypes.SET_USER:
+      return {
+        ...state,
+        isLoggedIn: true,
+        current_user: {
+          ...state.current_user,
+          ...action.user
+        }
+      };
+    case actionTypes.SET_RECENTLY_PLAYED:
+      return {
+        ...state,
+        recently_played: action.recently_played
+      };
+    case actionTypes.SET_PLAY_NOW:
+      return {
+        ...state,
+        play_now: {
+          type: action.uri_type,
+          uri: action.uri
+        }
+      };
+    case actionTypes.RESET_PLAY_NOW:
+      return {
+        ...state,
+        play_now: {
+          type: null,
+          uri: null
+        }
+      };
+    case actionTypes.SET_CURRENTLY_PLAYING:
+      return {
+        ...state,
+        currently_playing: action.song
+      };
+    case actionTypes.SET_IS_PLAYING:
+      return {
+        ...state,
+        isPlaying: action.isPlaying
+      };
+    case actionTypes.SET_BACKGROUND_IMAGE:
+      return {
+        ...state,
+        backgroundImage: action.backgroundImage
+      };
+    case actionTypes.PLAY_SONG_START:
+      return {
+        ...state,
+        play_now: null
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
